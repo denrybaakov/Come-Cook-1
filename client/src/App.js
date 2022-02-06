34c3ecd195dfc26f237037bc2c90e8427635029b
Ivan <vanezzo@ya.ru>
2022-02-06 17:17:06 +0300

    before dev dowload

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Main from './components/MainPage/Main';
 import SignIn from './components/Registration/SignIn/SignIn'
 
 import UserIndex from './components/UserPage/UserIndex/UserIndex'
+import MainPageTwo from "./components/MainPageTwo/MainPageTwo";
+
 
 
 function App() {
@@ -15,9 +17,11 @@ function App() {
       <Route path='/' element={<Main />}></Route>
       <Route path='/auth/login' element={<SignIn />}></Route>
       <Route path='/profile' element={<UserIndex />}></Route>
-      {/* <Route path='/profile/search' element={<UserIndex />}></Route>
+      <Route path='/2' element={<MainPageTwo />}></Route>
+    </Routes>
+      /* <Route path='/profile/search' element={<UserIndex />}></Route>
       <Route path='/profile/order' element={<UserIndex />}></Route>
-      <Route path='/profile/completed' element={<UserIndex />}></Route> */}
+      <Route path='/profile/completed' element={<UserIndex />}></Route> */
   );
 }
 
---
+++ client/src/components/MainPageTwo/MainPageTwo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function MainPageTwo() {
+  return (
+
+    <>
+    <h1>HELLO</h1>
+    </>
+
+  );
+}
