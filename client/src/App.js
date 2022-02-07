32142cb164864d93aa577b92742e75329b384c8c
Ivan <vanezzo@ya.ru>
2022-02-07 15:58:13 +0300

    before merge

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Main from './components/MainPage/Main';
 import SignIn from './components/Registration/SignIn/SignIn'
 import SignUp from './components/Registration/SignUp/SignUp';
+import Tests from './components/Tests/Tests';
 import UserIndex from './components/UserPage/UserIndex/UserIndex'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route path='/auth/login' element={<SignIn />}></Route>
       <Route path='/auth/register' element={<SignUp />}></Route>
       <Route path='/profile' element={<UserIndex />}></Route>
+      <Route path='/tests' element={<Tests />}></Route>
     </Routes>
   );
 }
---
+++ client/src/components/Tests/Tests.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Tests() {
+  return (
+    <>
+      <h2 style={{ color: "black" }} >
+        ТЕСТЫ
+      </h2>
+    </>
+  );
+}
