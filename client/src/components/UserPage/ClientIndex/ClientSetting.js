7e14cd5af2161de476ecc8e226f63b6e5caa8cab
Pavel <t1.saifar@gmail.com>
2022-02-09 20:49:18 +0300

    merge

--- client/src/components/UserPage/ClientIndex/ClientSetting.js
+++ client/src/components/UserPage/ClientIndex/ClientSetting.js
@@ -1,23 +1,27 @@
+import { useState } from "react"
 import { useSelector } from "react-redux"
 
 const ClientSettings = () => {
 
-  const { name, surname, email, password, about, phone, avatar } = useSelector(state => state.user)
-  console.log(name);
-  console.log(email);
+  // const [input, setInput] = useState({ avatar, name, about, surname, email, phone, password })
 
+  // const inputHandler = (e) => {
+  //   setInput(prev => ({ ...prev, [e.target.name]: e.target.value }));
+  // }
+
+  // const dispatch = useDispatch();
 
   return (
     <div className="col-70 profile__col-70">
       <h2 className="profile__title">Настройки</h2>
       <div className="setting">
-        <input type="text" className="input-profile setting__input" name="avatar" placeholder={`${avatar}`} />
-        <input type="text" className="input-profile setting__input" name="name" placeholder={`${name}`} />
-        <input type="text" className="input-profile setting__input" name="about" placeholder={`${about ?? "не заполнено"}`} />
-        <input type="text" className="input-profile setting__input" name="surname" placeholder={`${surname ?? "не заполнено"} `} />
-        <input type="text" className="input-profile setting__input" name="email" placeholder={`${email}`}  />
-        <input type="text" className="input-profile setting__input" name="phone" placeholder={`${phone}`} />
-        <input type="password" className="input-profile setting__input" name="password" placeholder={`${password}`} />
+        <input type="text" className="input-profile setting__input" name="avatar"  />
+        <input type="text" className="input-profile setting__input" name="name"  />
+        <input type="text" className="input-profile setting__input" name="about"  />
+        <input type="text" className="input-profile setting__input" name="surname"  />
+        <input type="text" className="input-profile setting__input" name="email"   />
+        <input type="text" className="input-profile setting__input" name="phone"  />
+        <input type="password" className="input-profile setting__input" name="password"  />
         <button className="btn-secondary setting__btn">Применить</button>
       </div>
     </div>
